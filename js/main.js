a3fb5ab716749f6d4b2006a9e1d32f340967e31d
Phanit Chea <phanit.chea@student.passerellesnumeriques.org>
2023-12-16 11:28:35 +0700

    #9 display product

--- js/main.js
+++ js/main.js
@@ -1,253 +1,149 @@
+let tbody = document.querySelector('tbody');
+let dialog = document.querySelector('#questions-dialog');
+let addBtn = document.querySelector('.add-product');
+// addBtn.addEventListener('click',onAddQuestion)
+let editIndex = null;
+// DATA  ---------------------------------------------------------
 let products = [
     {
         id: 1,
-        name: "Blouse",
+        names: "Blouse",
         category: "men",
+        price:10,
         quantity: 1,
         stock: 10,
         sold: 5,
     },
     {
         id: 2,
-        name: "Shirt",
+        names: "Shirt",
         category: "woman",
+        price:10,
         quantity: 1,
         stock: 10,
         sold: 5,
     },
     {
         id: 3,
-        name: "Intimates",
+        names: "Intimates",
         category: "woman",
+        price:10,
         quantity: 1,
         stock: 10,
         sold: 5,
-    },
-    {
-        id: 4,
-        name: "Bodysuit",
-        category: "men",
-        quantity: 1,
-        stock: 10,
-        sold: 4,
-    },
-    {
-        id: 5,
-        name: "Corset",
-        category: "men",
-        quantity: 1,
-        stock: 10,
-        sold: 5,
-    },
-    {
-        id: 6,
-        name: "Pashima",
-        category: "woman",
-        quantity: 1,
-        stock: 10,
-        sold: 2,
-    },
-    {
-        id: 7,
-        name: "Sport wear",
-        category: "woman",
-        quantity: 1,
-        stock: 10,
-        sold: 5,
-    },
-    {
-        id: 8,
-        name: "Bra",
-        category: "man",
-        quantity: 1,
-        stock: 10,
-        sold: 1,
-    },
-    {
-        id: 9,
-        name: "Caftan",
-        category: "men",
-        quantity: 1,
-        stock: 10,
-        sold: 5,
-
-    },
-    {
-        id: 10,
-        name: "Shorts",
-        category: "men",
-        quantity: 1,
-        stock: 10,
-        sold: 9,
-    },
-    {
-        id: 11,
-        name: "Boots",
-        category: "men",
-        quantity: 1,
-        stock: 15,
-        sold: 6,
-    },
-    {
-        id: 12,
-        name: "Sweater",
-        category: "woman",
-        quantity: 1,
-        stock: 10,
-        sold: 5,
-
-    },
-    {
-        id: 13,
-        name: "Skirt",
-        category: "woman",
-        quantity: 1,
-        stock: 10,
-        sold: 4,
-    },
-    {
-        id: 14,
-        name: "Vest",
-        category: "men",
-        quantity: 1,
-        stock: 10,
-        sold: 4,
-    },
-    {
-        id: 15,
-        name: "Socks",
-        category: "men",
-        quantity: 1,
-        stock: 10,
-        sold: 5,
-    },
-    {
-        id: 16,
-        name: "Cowl Neck Top",
-        category: "men",
-        quantity: 1,
-        stock: 10,
-        sold: 4,
-    },
-    {
-        id: 17,
-        name: "Jeans",
-        category: "men",
-        quantity: 1,
-        stock: 10,
-        sold: 4,
-    },
-    {
-        id: 18,
-        name: "Dress",
-        category: "woman",
-        quantity: 1,
-        stock: 10,
-        sold: 2,
-    },
-    {
-        id: 19,
-        name: "Long coat",
-        category: "men",
-        quantity: 1,
-        stock: 10,
-        sold: 5,
-    },
-    {
-        id: 20,
-        name: "Full skirt dress",
-        category: "woman",
-        quantity: 1,
-        stock: 10,
-        sold: 5,
-    },
-    {
-        id: 21,
-        name: "A-line skirt",
-        category: "woman",
-        quantity: 1,
-        stock: 10,
-        sold: 5,
-    },
-    {
-        id: 22,
-        name: "Suit jacket",
-        category: "man",
-        quantity: 1,
-        stock: 10,
-        sold: 5,
-    },
-    {
-        id: 23,
-        name: "Polo shirt",
-        category: "man",
-        quantity: 1,
-        stock: 10,
-        sold: 5,
-    },
-    {
-        id: 24,
-        name: "Khakis",
-        category: "man",
-        quantity: 1,
-        stock: 10,
-        sold: 5,
-    },
-    {
-        id: 25,
-        name: "Henley Shirt",
-        category: "man",
-        quantity: 1,
-        stock: 10,
-        sold: 5,
-    },
-    {
-        id: 26,
-        name: "Blazer",
-        category: "man",
-        quantity: 1,
-        stock: 10,
-        sold: 5,
-    },
-    {
-        id: 27,
-        name: "T-shirt",
-        category: "woman",
-        quantity: 1,
-        stock: 10,
-        sold: 5,
-    },
-    {
-        id: 28,
-        name: "Jacket",
-        category: "woman",
-        quantity: 1,
-        stock: 10,
-        sold: 5,
-    },
-    {
-        id: 29,
-        name: "Hoddie",
-        category: "man",
-        quantity: 1,
-        stock: 10,
-        sold: 5,
-    },
-    {
-        id: 30,
-        name: "Frock",
-        category: "woman",
-        quantity: 1,
-        stock: 10,
-        sold: 5,
-    },
+    }
 
 ]
 
-function saveProducts() {
-
-    localStorage.setItem("products",JSON.stringify(products)) // to add question to local storage by converting data to JSON format
-  
+// HIDE / SHOW ---------------------------------------------------------
+function hide(element) {
+    element.style.display = "none";
 }
-saveProducts()
 
+function show(element) {
+    element.style.display = "block";
+}
+
+//  LOCAL STORAGE ---------------------------------------------------------
+function saveProduct() {
+    localStorage.setItem("products", JSON.stringify(products));
+}
+
+function laodProduct() {
+    let questionsStorage = JSON.parse(localStorage.getItem("products"));
+    if (questionsStorage !== null) {
+        products = questionsStorage;
+    }
+}
+
+
+
+function randerProduct() {
+
+   
+    tbody.textContent='';
+    for (let i = 0; i < products.length; i++) {
+        let trbody = document.createElement('tr');
+        trbody.dataset.id = i;
+
+        let tdId = document.createElement('td');
+        tdId.textContent = products[i].id;
+        tdId.style.background = 'teal'
+
+        let tdnames = document.createElement('td');
+        tdnames.textContent = products[i].names;
+
+        let tdCategory = document.createElement('td');
+        tdCategory.textContent = products[i].category;
+        tdCategory.style.background = 'teal';
+
+        let tdPrice = document.createElement('td');
+        tdPrice.textContent = products[i].price;
+        console.log(tdPrice)
+
+        let tdQuantity = document.createElement('td');
+        tdQuantity.textContent = products[i].quantity;
+
+        let tdStock = document.createElement('td');
+        tdStock.textContent = products[i].stock;
+
+        let tdSold = document.createElement('td');
+        tdSold.textContent = products[i].sold;
+
+        let tdAction = document.createElement('td');
+        tdAction.classList.add('action');
+
+        let delete_div = document.createElement('div');
+        let delete_icon = document.createElement('i');
+        delete_div.classList.add('delete');
+        delete_icon.classList.add('material-icons');
+        delete_icon.textContent = 'delete';
+        delete_div.appendChild(delete_icon);
+        // delete_div.addEventListener('click',removeQuestion)
+
+        let edit_div = document.createElement('div');
+        edit_div.classList.add('edit');
+        let edit_icon = document.createElement('i');
+        edit_icon.classList.add('material-icons')
+        edit_icon.textContent = "edit";
+        edit_div.appendChild(edit_icon);
+        // edit_div.addEventListener('click', editQuestion);
+
+        let view_div = document.createElement('div');
+        view_div.classList.add('view');
+        let view_icon = document.createElement('i');
+        view_icon.classList.add('material-icons');
+        view_icon.textContent = "visibility";
+        view_div.appendChild(view_icon)
+
+        let chart_div = document.createElement('div');
+        chart_div.classList.add('chart');
+        let chart_icon = document.createElement('i');
+        chart_icon.classList.add('material-icons')
+        chart_icon.textContent = 'shopping_cart';
+        chart_div.appendChild(chart_icon)
+
+        tdAction.appendChild(delete_div);
+        tdAction.appendChild(edit_div);
+        tdAction.appendChild(view_div);
+        tdAction.appendChild(chart_div);
+
+        trbody.appendChild(tdId);
+        trbody.appendChild(tdnames);
+        trbody.appendChild(tdCategory);
+        trbody.appendChild(tdPrice);
+        trbody.appendChild(tdQuantity);
+        
+        trbody.appendChild(tdStock);
+        trbody.appendChild(tdSold);
+        trbody.appendChild(tdAction)
+
+        tbody.appendChild(trbody);
+    }
+    saveProduct()
+}
+
+
+laodProduct();
+
+randerProduct();
