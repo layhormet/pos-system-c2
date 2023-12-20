067eaedb2e032fd23d41319f0973157888dcdf68
Phanit Chea <phanit.chea@student.passerellesnumeriques.org>
2023-12-20 10:55:50 +0700

    #6 change local and storage

--- js/main.js
+++ js/main.js
@@ -1,2 +1,23 @@
+function saveProducts() {
+    // save data to localStorage 
+    localStorage.setItem('productsData', JSON.stringify(productsData));
+}
 
+function loadProducts() {
+    // load data from localStorage 
+    let loadProducts = JSON.parse(localStorage.getItem('productsData'));
+    if (loadProducts != undefined) {
+        productsData = loadProducts
+    }
+    else {
+        saveProducts();
+    }
+}
+let productsData = {
+    products: [],
+    latestId: null
+};
+loadProducts()
+
+renderProducts()
 
